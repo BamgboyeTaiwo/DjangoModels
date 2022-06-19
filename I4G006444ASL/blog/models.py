8e75317ea79dc6473df3a7b3c77601a36631a23a
Bamgboye Taiwo <102883093+BamgboyeTaiwo@users.noreply.github.com>
2022-06-19 14:46:41 +0100

    model added

--- I4G006444ASL/blog/models.py
+++ I4G006444ASL/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200, unique=True)
+    Text = models.SlugField(max_length=200, unique=True)
+    Author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='posts')
+    Published_date = models.DateTimeField(auto_now= True)
+    Created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
